afb9e623ccfaa5f105d2a66f2af02604bce1878b
Aifan786 <101566156+Aifan786@users.noreply.github.com>
2022-05-17 22:52:31 +0530

    Add files via upload
    total_run

---
+++ Score.java
@@ -0,0 +1,16 @@
+package Sprint1;
+
+public class Score {
+
+	public static void main(String[] args) {
+		
+		int a = 4;
+		int b = 2;
+		int c = 1;
+		int d = 4;
+		int e = 6;
+		int score = 1*a+2*b+3*c+4*d+6*e;
+		System.out.println(score);
+		
+	}
+}
